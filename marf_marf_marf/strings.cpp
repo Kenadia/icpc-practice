628558ce6b729f0a589298b2a86e131e8ffe5ca6
Marielle Foster <marielleefoster@gmail.com>
2015-03-21 10:22:39 -0500

    string discoveries

--- marf_marf_marf/strings.cpp
+++ marf_marf_marf/strings.cpp
@@ -6,7 +6,16 @@ using namespace std;
 
 int main()
 {
-	string stringz = "Fun with Strings";
-	cout << mystring;
+	string stringz = "Fun with Strings \n";
+	cout << stringz;
+
+	cout << stringz[0];
+	cout << "\n";
+
+	//what is endl?
+	string marf = "ding";
+
+	//obviously a newline thing
+	cout << marf << endl;
 	return 0;
 }
