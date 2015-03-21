f5435ba9b665f3284940b91c028e2d9f5d673d14
Marielle Foster <marielleefoster@gmail.com>
2015-03-21 10:12:23 -0500

    printing

---
+++ marf_marf_marf/printing.cpp
@@ -0,0 +1,12 @@
+//commenting for funzz
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Marf marf marf marf";
+	cout << "Programming for the world championships..."
+	cout << "Casual"
+	return 0;
+}
