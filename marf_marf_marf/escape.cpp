3c3b724d43aae1eaf9352bc98b23690467441adc
Marielle Foster <marielleefoster@gmail.com>
2015-03-21 10:30:02 -0500

    escape characters
    having fun with escape characters in cpp

---
+++ marf_marf_marf/escape.cpp
@@ -0,0 +1,24 @@
+//escape.cpp
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "newline";
+    cout << "\n";
+    cout << "carriage return";
+    cout << "\r";
+    cout << "vertical tab";
+    cout << "\v";
+    cout << "Form feed";
+    cout << "\f";
+    cout << "Alert (beep)";
+    cout << "\a";
+    // cout << "\";
+    // cout << "\";
+    // cout << "\";
+    // cout << "\";
+    return 0;
+}
